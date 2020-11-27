b8a6f835ea78575533f436a08d8c4690c5240ca2
RicardoReyes44 <rickyboy1002@gmail.com>
2020-11-27 14:35:45 -0600

    Se agrego la implementacion de generar numeros aleatorios en el metodo estatico 'generarMilNumeros' de la clase de prueba

--- src/Prueba.java
+++ src/Prueba.java
@@ -2,7 +2,13 @@
 public class Prueba {
 
 	public static int[] generarMilNumeros() {
+        int []array = new int[1000];
 		
+		for(int i=0; i<1000; i++) {
+			array[i] = (int)(Math.random()*100);
+		}
+		
+		return array;
 	}
 	
 	public static void main(String[] args) {
