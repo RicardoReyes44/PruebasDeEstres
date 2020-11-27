690a57f5a3b270a68a082b3cdb50cc3ce285a8c1
RicardoReyes44 <rickyboy1002@gmail.com>
2020-11-27 11:22:45 -0600

    Se agrego la implementacion del metodo burbuja en el metodo 'ordenacionBurbuja1' dentro de la clase estatica interna 'Burbuja'

--- src/MetodosOrdenamiento.java
+++ src/MetodosOrdenamiento.java
@@ -6,6 +6,17 @@ public class MetodosOrdenamiento {
 	static class Burbuja{
 		public static void ordenacionBurbuja1(long []numeros) {
 			
+            for(int i=1; i<=numeros.length-1; i++) {
+				
+				for(int j=0; j<=numeros.length-i-1; j++) {
+					
+					if(numeros[j]>numeros[j+1]) {
+						long aux = numeros[j];
+						numeros[j] = numeros[j+1];
+						numeros[j+1] = aux;
+					}
+				}
+			}
 			
 		}        
 	}// class Burbuja
