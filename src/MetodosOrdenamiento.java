59adec388ff3117ef641ff342b8089bfa73af451
RicardoReyes44 <rickyboy1002@gmail.com>
2020-11-27 17:03:48 -0600

    Se agrego la implementacion del metodo radix en el metodo estatico 'orden' de la clase estatica interna 'Radix'

--- src/MetodosOrdenamiento.java
+++ src/MetodosOrdenamiento.java
@@ -143,7 +143,36 @@ public class MetodosOrdenamiento {
 	
     static class Radix{
     	public static void orden(long[]numeros) {
-    		
+    	      if(numeros.length == 0)
+    	          return;
+    	          int[][] np = new int[numeros.length][2];
+    	          int[] q = new int[0x100];
+    	          int i,j,k,l,f = 0;
+    	          for(k=0;k<4;k++) {
+    	             for(i=0;i<(np.length-1);i++)
+    	             np[i][1] = i+1;
+    	             np[i][1] = -1;
+    	             for(i=0;i<q.length;i++)
+    	             q[i] = -1;
+    	             for(f=i=0;i<numeros.length;i++) {
+    	                j = (int)((0xFF<<(k<<3))&numeros[i])>>(k<<3);
+    	                if(q[j] == -1)
+    	                l = q[j] = f;
+    	             else {
+    	                l = q[j];
+    	                while(np[l][1] != -1)
+    	                l = np[l][1];
+    	                np[l][1] = f;
+    	                l = np[l][1];
+    	             }
+    	             f = np[f][1];
+    	             np[l][0] = (int)numeros[i];
+    	             np[l][1] = -1;
+    	          }
+    	          for(l=q[i=j=0];i<0x100;i++)
+    	          for(l=q[i];l!=-1;l=np[l][1])
+    	        	  numeros[j++] = np[l][0];
+    	       }//for
     	}//metodo
   	}//metodo
 	
