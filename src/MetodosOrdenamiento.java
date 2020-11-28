589a3de2277f711628dcd3b027256ccec5c2b09c
RicardoReyes44 <rickyboy1002@gmail.com>
2020-11-27 20:56:21 -0600

    Se agregaron las variables locales 'recorridos', 'intercambios' y 'comparaciones' en el metodo 'orden' de la clase interna 'Radix'

--- src/MetodosOrdenamiento.java
+++ src/MetodosOrdenamiento.java
@@ -260,38 +260,66 @@ public class MetodosOrdenamiento {
 	
     static class Radix{
     	public static long[] orden(long[]numeros) {
+    		int comparaciones = 0;
+			int intercambios = 0;
+			int recorridos = 0;
     		  tInicio = System.nanoTime();
-    	      if(numeros.length == 0)
+    	      if(numeros.length == 0) {
+    	    	  comparaciones++;
     	          return null;
+    	      }
     	          int[][] np = new int[numeros.length][2];
     	          int[] q = new int[0x100];
     	          int i,j,k,l,f = 0;
     	          for(k=0;k<4;k++) {
-    	             for(i=0;i<(np.length-1);i++)
+    	        	  recorridos++;
+    	        	  comparaciones++;
+    	             for(i=0;i<(np.length-1);i++) {
+    	             recorridos++;
     	             np[i][1] = i+1;
+    	             comparaciones++;
+    	             }
     	             np[i][1] = -1;
-    	             for(i=0;i<q.length;i++)
+    	             for(i=0;i<q.length;i++) {
+    	             recorridos++;
     	             q[i] = -1;
-    	             for(f=i=0;i<numeros.length;i++) {
+    	             comparaciones++;
+    	             }for(f=i=0;i<numeros.length;i++) {
+    	            	 recorridos++;
+    	            	 comparaciones++;
     	                j = (int)((0xFF<<(k<<3))&numeros[i])>>(k<<3);
+    	                comparaciones++;
     	                if(q[j] == -1)
     	                l = q[j] = f;
     	             else {
     	                l = q[j];
-    	                while(np[l][1] != -1)
+    	                while(np[l][1] != -1) {
     	                l = np[l][1];
+    	                recorridos++;
+    	                }
+    	                intercambios++;
     	                np[l][1] = f;
     	                l = np[l][1];
+    	                comparaciones++;
     	             }
     	             f = np[f][1];
     	             np[l][0] = (int)numeros[i];
     	             np[l][1] = -1;
     	          }
-    	          for(l=q[i=j=0];i<0x100;i++)
-    	          for(l=q[i];l!=-1;l=np[l][1])
+    	          for(l=q[i=j=0];i<0x100;i++) {
+    	        	  comparaciones++;
+    	        	  recorridos++;
+    	          for(l=q[i];l!=-1;l=np[l][1]) {
     	        	  numeros[j++] = np[l][0];
+    	        	  comparaciones++;
+    	        	  recorridos++;
+    	          }
+    	          }
     	       }//for
     	       tFin = System.nanoTime();
+    	       System.out.println("Cantidad de recorridos: " + recorridos);
+               System.out.println("Cantidad de intercambios: " + intercambios);
+               System.out.println("Cantidad de comparaciones: " + comparaciones);
     		   System.out.println("Tiempo de ejecucion en ordenamiento: " + (tFin-tInicio));
     	    return numeros;
     	}//metodo
